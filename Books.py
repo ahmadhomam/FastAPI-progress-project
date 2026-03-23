6594596b960bc866d971b8379ea1c0b86698395d
Homam Ahmad Kazimi <ahmadhomam38@gmail.com>
2026-03-24 04:17:50 +0530

    Dynamic Path Parameter, no spaces in path and get operation

--- Books.py
+++ Books.py
@@ -2,6 +2,18 @@ from fastapi import FastAPI     # importing the dependencies FastAPI from fastap
 
 app = FastAPI()                 # help the uvicorn(surver) to identify this application is FastAPI
 
-@app.get("/api-endpoint")
-async def first_api() :         # python function 
-    return {"message" : "Hello User !"}
+
+books = [
+    {"title": "Spider_man","Author": "Stan Lee", "category": "Comics"},
+    {"title": "Amazing Spider_man","Author": "Stan Lee", "category": "Comics"},
+    {"title": "Harry Potter one","Author": "J.K Rowling", "category": "Comics"},
+    {"title": "Harry Potter two","Author": "J.K Rowling", "category": "Comics"},
+    {"title": "Half Girlfriend","Author": "Chetan Bagat", "category": "Romantic"},
+]
+
+
+@app.get("/books/{title_name}")
+async def read_books(title_name:str) :         # python function 
+    for book in books:
+        if book.get("title").casefold() == title_name.casefold() :
+            return book
