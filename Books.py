94296bb1ac59efecea2183df9f93d5967b8e1f72
Homam Ahmad Kazimi <ahmadhomam38@gmail.com>
2026-03-26 01:58:16 +0530

    Query Parameter

--- Books.py
+++ Books.py
@@ -4,16 +4,29 @@ app = FastAPI()                 # help the uvicorn(surver) to identify this appl
 
 
 books = [
-    {"title": "Spider_man","Author": "Stan Lee", "category": "Comics"},
-    {"title": "Amazing Spider_man","Author": "Stan Lee", "category": "Comics"},
+    {"title": "Spider man","Author": "Stan Lee", "category": "Comics"},
+    {"title": "Amazing Spider man","Author": "Stan Lee", "category": "Comics"},
     {"title": "Harry Potter one","Author": "J.K Rowling", "category": "Comics"},
     {"title": "Harry Potter two","Author": "J.K Rowling", "category": "Comics"},
     {"title": "Half Girlfriend","Author": "Chetan Bagat", "category": "Romantic"},
 ]
 
 
-@app.get("/books/{title_name}")
-async def read_books(title_name:str) :         # python function 
+# @app.get("/books/{title_name}")
+# async def read_books(title_name:str) :         # python function 
+#     for book in books:
+#         if book.get("title").casefold() == title_name.casefold() :
+#             return book
+
+
+#query parameter
+
+@app.get("/{book_title}/")
+async def book_for_query(book_title:str,category:str) :
+    book_to_show = []
     for book in books:
-        if book.get("title").casefold() == title_name.casefold() :
-            return book
+        if (book.get("title").casefold() == book_title.casefold() and 
+            book.get("category").casefold() == category.casefold()):
+            book_to_show.append(book)
+    
+    return book_to_show
